a76929f9bdffedaa5cda2cddf1fddc678372c794
George Lesica <george@lesica.com>
2020-07-20 13:26:12 -0600

    Update build script to push

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+set -e
+
 # Rebuild the Docker image from the current Dockerfile and push it to the Hub.
 # This requires being logged into Docker Hub with sufficient permissions within
 # the organization.
 
-docker build -t traviswheelerlab/gnina:builderror .
+TAG_VERSION=${TAG_VERSION:-"local"}
+
+docker build \
+    -t "traviswheelerlab/gnina:latest" \
+    -t "traviswheelerlab/gnina:$TAG_VERSION" \
+    .
+
+if [ "$TAG_VERSION" == "local" ]; then
+    echo "Skipping push for local build"
+    exit 0
+fi
+
+docker push "traviswheelerlab/gnina:$TAG_VERSION"
 
